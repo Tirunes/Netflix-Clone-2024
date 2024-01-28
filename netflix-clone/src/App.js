9da79a82ab37fa0a0ae5b4cc5c3863cf40431935
Tirunes <147859703+Tirunes@users.noreply.github.com>
2024-01-28 14:18:45 -0800

    header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,12 +1,18 @@
 
 import './App.css';
+import Home from './Pages/Homes/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+<Home/>
+    
     </div>
   );
 }
 
 export default App;
+
+// my netflix Api number
+// 8742e4577bd36cae006921fe02851a2d
